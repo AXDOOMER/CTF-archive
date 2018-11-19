4e8154d632dd2e07b67a6a1a463a1a6b2dcb1e3b
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2018-11-19 18:43:43 -0500

    nosleep.c: Fix indentation

--- tools/nosleep.c
+++ tools/nosleep.c
@@ -28,8 +28,10 @@ int strcmp(const char* s1, const char* s2)
 	printf("\ns1: %s\n", s1);
 	printf("\ns2: %s\n", s2);
 
-	while (*s2 == *s1) {
-	if (*s1 == '\0') return 0;
+	while (*s2 == *s1)
+	{
+		if (*s1 == '\0')
+			return 0;
 		s1++;
 		s2++;
 	}
